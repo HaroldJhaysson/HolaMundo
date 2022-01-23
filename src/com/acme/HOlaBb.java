d28d4c8c432b2c86c06cf1ee8ecb68561163a693
HaroldFicct <Harold.apaza.monasterio@gmail.com>
2022-01-23 02:20:26 -0100

    se creo la clase persona

--- src/com/acme/HOlaBb.java
+++ src/com/acme/HOlaBb.java
@@ -4,6 +4,12 @@ public class HOlaBb {
     public static void main(String[] args) {
         Mensaje msg = new Mensaje();
         String nombre = msg.SolicitarDatos("escribe tu nombre: ");
-        msg.saludar(nombre);
+        String apellido = msg.SolicitarDatos("escribe tu apellido: ");
+        Persona p1 = new Persona(nombre,apellido);
+        msg.saludar(p1);
+
+
+
     }
+
 }
